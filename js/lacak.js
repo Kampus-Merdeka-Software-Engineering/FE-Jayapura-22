16303a139a82b3bd2f8113279ab425ba4b3f4ea0
Irma116 <irmanurhalimah116@gmail.com>
2023-10-05 20:39:51 +0700

    Update Commit

--- js/lacak.js
+++ js/lacak.js
@@ -1,3 +1,76 @@
+const BASE_URL = "be-jayapura-22-production.up.railway.app"
+
+function handleClick(event){
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("showNavbar");
+    navbar.classList.toggle("hideNavbar");
+}
+
+function lacak(event){
+    const inputNama = document.querySelector("#name");
+    const inputNamaBarang = document.querySelector("#namabarang");
+    const inputNoHP = document.querySelector("#nohp");
+    const inputDestination = document.querySelector("#destination");
+    const inputweight = document.querySelector("weight");
+    
+    const valueNama = inputNama.value;
+    const valueNamaBarang = inputNamaBarang.value;
+    const valueNomorHP = inputNoHP.value;
+    const valueDestination = inputDestination.value;
+    const valueWeight = inputweight.value
+
+    fetch(`${BASE_URL}/input-data-barang`,{
+      method: "POST",
+      body: JSON.stringify ({
+        nama: valueNama,
+        nomorBarang: valueNamaBarang,
+        nomorHp: valueNomorHP,
+        destination: valueDestination,
+        weight: valueWeight,
+      }), 
+
+    })
+    .then((Response) => Response.json())
+    .then((Response) => {
+      window.alert(
+        `Berhasil menyimpan data, nomor tracking anda ${response.nomorTracking}`
+        );
+      inputNama.value = "";
+      inputNamaBarang.value = "";
+      inputNoHP.value = "";
+      inputDestination.value = "";
+      inputweight.value = "";
+    })
+    .catch.error (error => {
+        console.error(error);
+
+    });
+}
+
+function tracking(event) {
+    const inputTrackinNumber = document.querySelector("#tracking-number")
+    const value = inputTrackinNumber.value
+    fetch('$BASE_URL)/cek-shipping/${value}')
+        .then((Response) => Response.json())
+        .then((Response) => {
+          inputTrackinNumber.value; "";
+          const info = document.querySelector (".info");
+          info.innerHTML = `
+            <ul>
+                <li>nama: ${Response.data.nama}</li>
+                <li>nama Barang: ${Response.data.namaBarang}</li>
+                <li>nomor Hp: ${Response.data.nomorHp}</li>
+                <li>destination: ${Response.data.destination}</li>
+                <li>weight: ${Response.data.weight}</li>
+            </ul>
+          `;
+          
+    })
+    .catch.error (error => {
+        console.error(error);
+    });
+
+}
 
 function handleClick(event){
     const navbar = document.querySelector(".navbar");
